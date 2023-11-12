b682d56f62bcd4ea45773166c5a471131cf5c44e
xueqingw@bu.edu <xueqingw@bu.edu>
2023-11-12 01:54:47 +0000

    Fix query

--- mylib/sample_query.py
+++ mylib/sample_query.py
@@ -1,4 +1,4 @@
-SELECT merged_tables.year, SUM(births) AS total_birth
+SELECT year, SUM(births) AS total_birth
 FROM (
     SELECT year, births
     FROM birth2000_delta b2000
@@ -7,4 +7,4 @@ FROM (
     FROM birth1994_delta b1994
 ) AS merged_tables 
 GROUP BY year
-ORDER BY year
+ORDER BY year;
